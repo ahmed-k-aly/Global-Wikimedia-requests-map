e406d4f8a217cb0e638125b667745cbb5ff7364f
Ahmed Aly <aaly24@amherst.edu>
2021-07-20 11:16:01 -0400

    New way to export to the csv

--- mapCountryData.py
+++ mapCountryData.py
@@ -1,24 +1,61 @@
+""" 
+This program filters the wikipedia API country's pageviews data.
+It reads all the JSON files that have country data and exports 
+them to a single CSV with the dates, the countries, and the data.
+"""
+
+
+import numpy as np
 import os
 import json
 from datetime import datetime
 import csv
 
+
 def main():
     filesList = os.listdir('pageCountsByCountry')
-    dataList: list = getDataDict(filesList)
+    dataList: list = getDataList(filesList)
     dates, countries = getRowsCols(dataList)
-    write_to_csv(dates, countries, dataList)
-    
-    
-    
-def write_to_csv(dates: list, countries: list, dataList: list):
-    with open('requestsPerCountry.csv', 'w', newline='') as csvfile:
-        fieldnames = ['date'] + countries
-        theWriter = csv.DictWriter(csvfile, fieldnames = fieldnames)
+    #write_to_csv(dates, countries, dataList)
+    new_write_to_csv(dates, countries, dataList)
+
+
+def new_write_to_csv(dates: list, countries: list, dataList: list):
+    """ 
+    This method exports the data in a CSV, but in a different format than
+    the original write to csv method. The new format should be easier to
+    handle with most of the plotting libraries. The format that the new
+    CSV is in is Countries,Requests,Dates.
+    """
+
+    with open('newRequests_per_country.csv', 'w', newline='') as csvfile:
+        fieldnames = ["Country", "Requests", "Dates"]
+        theWriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
         theWriter.writeheader()
         for index in dataList:
-            theWriter.writerow(index)                
-                
+            for key in index:
+                if key == 'date':
+                    continue
+                theWriter.writerow(
+                    {"Country": key, "Dates": index['date'], "Requests": index[key]})
+
+
+def write_to_csv(dates: list, countries: list, dataList: list):
+    """ 
+    Exports the data to a CSV in the format of
+            Country A,      CountryB,       etc...
+    Date1,  numRequestsA,   numRequestsB,   etc...
+    Date2,  numRequestsA,   numRequestsB,   etc...
+    etc...
+    """
+    with open('requestsPerCountry.csv', 'w', newline='') as csvfile:
+        fieldnames = ['date'] + countries
+        theWriter = csv.DictWriter(
+            csvfile, fieldnames=fieldnames, restval=np.nan)
+        theWriter.writeheader()
+        for index in dataList:
+            theWriter.writerow(index)
+
 
 def getRowsCols(dataList: list) -> list:
     dateList = []
@@ -32,15 +69,25 @@ def getRowsCols(dataList: list) -> list:
                 countryList.append(key)
     return dateList, countryList
 
-def getDataDict(jsonList) -> list:
-    allPagesDict: list = []
+
+def getDataList(jsonList) -> list:
+    """ 
+    Takes a list of all the files and returns a list of the number of
+    requests, the country's name, and the date. The form the data 
+    is arranged in is that every month is in an index of that list.
+    The indices themselves are a dictionary with the 'first' entry as
+    the date, and the rest are key: value pairs of country: requests.
+    """
+
+    allPagesList: list = []
     for file in jsonList:
-        json_file = open('pageCountsByCountry/' +file, 'r')
+        json_file = open('pageCountsByCountry/' + file, 'r')
         data: dict = json.load(json_file)
         json_file.close()
         oneJsonsDict: dict = getOneFilesData(data)
-        allPagesDict.append(oneJsonsDict)
-    return allPagesDict    
+        allPagesList.append(oneJsonsDict)
+    return allPagesList
+
 
 def getOneFilesData(json: dict) -> dict:
     """ 
@@ -59,8 +106,8 @@ def getOneFilesData(json: dict) -> dict:
     return pageDict
 
 
-def getOneCountriesData(countriesData: dict)-> dict:    
-    return {countriesData["country"] : countriesData["views_ceil"]}
+def getOneCountriesData(countriesData: dict) -> dict:
+    return {countriesData["country"]: countriesData["views_ceil"]}
 
 
 if __name__ == '__main__':
