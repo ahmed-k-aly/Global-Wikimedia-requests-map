39aaf59e4f51e680ac7e57fc789728fc64fa1cc0
Ahmed Aly <aaly24@amherst.edu>
2021-07-27 17:49:39 -0500

    fix

--- plotData.py
+++ plotData.py
@@ -6,35 +6,101 @@ import numpy as np
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
+from pandas.core.frame import DataFrame
 import pycountry
 import contextily as ctx
+import ipywidgets
+import os
+from tkinter import *
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+
+
 
 def main():
+    dateToLookAt = '2015-06'
+    filesList = os.listdir('pageCountsByCountry')
+    for date in filesList:
+        df, world = readData()
+        df = cleanData(df, world)
+
+        date = date.replace('.json','')
+        chooseTimeInterval(df, date = date)
+        plotData(df, date)
+
+
+def checkForWrongDateInputs(date: str):
+    """ 
+    Helper Method that raises exception if an error is 
+    caught relating to how the date was passed.
+    """
+    if type(date) != str:
+        raise TypeError("Date type is {} instead of str".format(type(date)))
+    elif len(date) != 7 or date[4] != '-':
+        raise ValueError("Date is in the wrong format")
+    splitDate = date.split('-')
+    year = int(splitDate[0])
+    month = int(splitDate[1])
+    if year > 2021 or year < 2015:
+        raise ValueError("Data only available from 2015 to 2021")
+    elif year == 2015 and month < 5:
+        raise ValueError("Data only available from 2015-05")
+    elif year == 2021 and month >6:
+        raise ValueError("Data only available until 2021-06")
+    elif month > 12 or month < 1:
+        raise ValueError("Months are only between 1 and 12")
+
+def chooseTimeInterval(df, date: str):
+    """ 
+    Method that selects the data based on the date 
+    argument that's passed. 
+    Date to be entered as YYYY-MM
+    """
+    checkForWrongDateInputs(date)    
+    date += '-01 00:00:00'
+    df.query("Dates == '{}' or Dates != Dates".format(date), inplace=True)
+    df.reset_index(drop=True, inplace=True)
+
+
+
+def readData() -> DataFrame:    
+    """ 
+    Reads the csv data and the world data and returns them as a DataFrame
+    """
     df = pd.read_csv('newRequests_Per_Country.csv')    
     world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+    return df, world
+
+
+def cleanData(df: DataFrame, world):
+    """ 
+    Merges the dataFrame with the world dataSet
+    and cleans both dataFrames. 
+    """
     changeCountryISOcode(df)
     removeBadCountries(df)
     world = world.merge(df, left_on='iso_a3', right_on='Country',how='left')
     world.drop(labels=['pop_est', 'continent', 'gdp_md_est', 'Country',],axis=1, inplace=True)
-    world.query("Dates == '2015-05-01 00:00:00' or Dates != Dates", inplace=True)
     world.reset_index(drop=True, inplace=True)
+    return world
 
-    world.plot(column='Requests',cmap='YlOrRd',scheme = 'percentiles', figsize=(18,10), legend = True, edgecolor ='black', 
+
+def plotData(df: DataFrame, date: str, color: str = 'YlOrRd'):
+    """ 
+    Plots the data in a map
+    """
+    df.plot(column='Requests',cmap = color,scheme = 'percentiles', figsize=(18,10), legend = True, edgecolor ='black', 
             missing_kwds={
         "color": "lightgrey",
         "edgecolor": "red",
         "hatch": "///",
         "label": "Missing values",
     })
-    
-
     #removing axis ticks
     plt.axis('off')#Add the title
-    plt.title("Number of the Wikipedia pageviews per country")
-    plt.show()    
+    plt.title("Number of the Wikipedia pageviews per country on " + date)
+    plt.show()
     
-    
-        
 def removeBadCountries(df):
     """ 
     Removes Countries with unknown ISO codes
